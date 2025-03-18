cec940414b70da509fc994d15c55ba4bec4d31da
Diego <38155922+Diegomcha@users.noreply.github.com>
2025-03-18 11:04:56 +0100

    chore: added files for lab8

---
+++ src/symboltable/SymbolTable.java
@@ -0,0 +1,35 @@
+package symboltable;
+
+import java.util.*;
+
+import ast.definition.Definition;
+
+public class SymbolTable {
+
+    private int scope = 0;
+    private List<Map<String, Definition>> table;
+
+    public SymbolTable() {
+        // TODO: todo
+    }
+
+    public void set() {
+        // TODO: todo
+    }
+
+    public void reset() {
+        // TODO: todo
+    }
+
+    public boolean insert(Definition definition) {
+        // TODO: todo
+    }
+
+    public Definition find(String id) {
+        // TODO: todo
+    }
+
+    public Definition findInCurrentScope(String id) {
+        // TODO: todo
+    }
+}
